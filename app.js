53a7f0e2895d572d6f0c135d5bf877a68561a65a
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-08-21 14:53:32 -0400

    animation now plays in reverse when you click the modal x

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ const button = document.querySelector('.button');
 const modal = document.querySelector('.modal-container');
 
 button.addEventListener('click', function () {
-  console.log('hello');
-  console.log(explore);
   explore.classList.add('delete-explore');
-  console.log(modal);
   modal.classList.add('adding-modal');
 });
 
 const modalButton = document.querySelector('.modal-button');
 
-modalButton.addEventListener('click', function(){
+modalButton.addEventListener('click', function () {
+  explore.classList.add('returning-explore');
+  modal.classList.add('removing-modal');
+
+  setTimeout(() => {
     explore.classList.remove('delete-explore');
     modal.classList.remove('adding-modal');
-})
+    explore.classList.remove('returning-explore');
+    modal.classList.remove('removing-modal');
+  }, 1000);
+});
